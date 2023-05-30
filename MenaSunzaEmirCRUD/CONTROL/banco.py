0b3d370b8388818ee5ec92773811a0f19f2b482d
Emir Mena <emir2102mena@gmail.com>
2023-05-30 14:00:00 -0600

    Documentacion y patrones de diseno

--- MenaSunzaEmirCRUD/CONTROL/banco.py
+++ MenaSunzaEmirCRUD/CONTROL/banco.py
@@ -1,6 +1,13 @@
 from CONTROL.estado import *
 # Clase contexto
 class Banco:
+    __instance = None
+    def __new__(cls,*args, **kwargs):
+        if (cls.__instance is None):
+            cls.__instance = super(Banco,cls).__new__(cls)
+        return cls.__instance
+            
+
     def __init__(self,GUI):
         #Estado inicial del programa
         self.controlador=ControladorDatos()
